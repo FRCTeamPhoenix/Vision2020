c08e7d5e030627d45f323d4c1854a8126148b63e
Maeswoon <sendmasonmail@gmail.com>
2019-10-24 12:44:51 -0400

    Removed scripts

--- build.sh
+++
@@ -1,4 +0,0 @@
-rm -rf build && mkdir build
-cd build
-cmake -DBUILD_SHARED_LIBS=OFF ..
-make
--- install.sh
+++
@@ -1,8 +0,0 @@
-unzip allwpilib-master.zip
-mv allwpilib-master wpilib
-mv wpilib ~
-cd ~/wpilib
-mkdir build && cd build
-cmake -DWITHOUT_JAVA=ON ..
-make -j4
-sudo make install
